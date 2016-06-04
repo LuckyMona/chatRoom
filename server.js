f5fb54e338dea9449047d113948b94a8dc0cf985
Luckymona Tien <miao.hnlk@foxmail.com>
2016-06-04 21:02:44 +0800

    server.js

--- server.js
+++ server.js
@@ -13,3 +13,39 @@ var http = require("http"),
 		res.writeHead(200, {"Content-Type":mime.lookup(path.basename(filePath))});
 		res.end(fileCont);
 	}
+
+	function serveStatic(res, cache, absPath){
+		if(cache[absPath]){
+			sendFile(res, absPath, cache[absPath]);
+			return;
+		}
+		fs.exists(absPath, function(exists){
+			if(exists){
+				fs.readFile(absPath, function(err,data){
+					if(err){
+						send404(res);
+						return;
+					}
+					cache[absPath] = data;
+					sendFile(res, absPath, data);
+				})
+				return;
+			}
+			send404(res);
+		})
+	}
+
+	var server = http.createServer(function(req, res){
+		var filePath = false;
+		if(req.url == "/"){
+			filePath = 'public/index.html';
+		} else {
+			filePath = "public/" + req.url;
+		}
+		var absPath = './'+filePath;
+		serveStatic(res, cache, absPath);
+	})
+
+	server.listen(3000, function(){
+		console.log("server listening on port 3000")
+	});
